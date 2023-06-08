a9696cfdcd22821e6afd6ef5f7c124f1de6d903e
dobrakotyata <127792247+dobrakotyata@users.noreply.github.com>
2023-06-08 17:04:27 +0100

    create my package

---
+++ clean_folder/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='clean_folder',
+    version='1.0',
+    packages=['clean_folder'],
+    entry_points={
+        'console_scripts': [
+            'clean-folder = clean_folder.clean:main'
+        ]
+    }
+)
+
