e14e110ea7c63bc5f07fb1e12f30c9df15f51c61
dobrakotyata <127792247+dobrakotyata@users.noreply.github.com>
2023-06-08 16:25:18 +0100

    add func birthdays_week

---
+++ birthdays_week.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+
+def get_birthdays_per_week(users):
+    # Определяем текущую дату
+    today = datetime.now().date()
+
+    # Определяем дату начала недели (понедельник)
+    start_of_week = today - timedelta(days=today.weekday())
+
+    # Определяем дату конца недели (воскресенье)
+    end_of_week = start_of_week + timedelta(days=6)
+
+    # Создаем словарь, в котором ключами будут дни недели, а значениями - список именинников
+    birthdays_per_day = {}
+
+    # Итерируемся по пользователям
+    for user in users:
+        name = user['name']
+        birthday = user['birthday'].date()
+
+        # Проверяем, что день рождения пользователя находится в текущей неделе
+        if start_of_week <= birthday <= end_of_week:
+            # Определяем день недели дня рождения пользователя
+            weekday = birthday.strftime('%A')
+
+            # Добавляем пользователя в список именинников для соответствующего дня недели
+            birthdays_per_day.setdefault(weekday, []).append(name)
+
+    # Выводим список именинников по дням недели
+    for weekday, names in birthdays_per_day.items():
+        print(f'{weekday}: {", ".join(names)}')
+
